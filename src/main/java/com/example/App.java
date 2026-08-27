bacfc27a29af8049b1aa549a0d58eefa2c439d62
BrianMulcair <bmulcair@lawreform.ie>
2026-08-27 12:22:06 +0100

    Initial CodeArtifact demo

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+import com.example.library.Hello;
+
+public class App {
+	 public static void main(String[] args) {
+	        //System.out.println("Hello from CodeArtifact!");
+		 System.out.println(Hello.message());
+	    }
+}
